5996c6f90e6c4c2651294fbef257fa405105353b
ZininSerge <zinin@xakep.ru>
2011-09-04 01:23:02 +0800

    Include cache machine in action view

--- lib/action_view/helpers/cache_helper.rb
+++ lib/action_view/helpers/cache_helper.rb
@@ -9,4 +9,4 @@ module ActionView
     end
   end
 end
-ActionView::Helpers::CacheHelper.send :extend, ActionView::Helpers::CacheHelper::CacheMachine
+ActionView::Helpers::CacheHelper.send :include, ActionView::Helpers::CacheHelper::CacheMachine
