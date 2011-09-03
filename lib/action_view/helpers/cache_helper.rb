d8846d712a5f324a12b6ad7c726df3a71d8ac00c
ZininSerge <zinin@xakep.ru>
2011-09-04 01:38:51 +0800

    Add cache machine to autoload

--- lib/action_view/helpers/cache_helper.rb
+++ lib/action_view/helpers/cache_helper.rb
@@ -1,16 +1,10 @@
-require 'action_view/context'
-
 module ActionView
   module Helpers
     module CacheMachineHelper
-
-      extend ActiveSupport::Concern
-
       def cache_for record, cacheable, options = {}, &block
-        record.fetch_cache_of(cacheable, options) { capture &block }
+        record.fetch_cache_of(cacheable, options.merge(:format => :ehtml)) { capture &block }
       end
     end
   end
   ActionView::Helpers.autoload :CacheMachineHelper
-  ActionView::Helpers.send(:include, Helpers::CacheMachineHelper)
 end
