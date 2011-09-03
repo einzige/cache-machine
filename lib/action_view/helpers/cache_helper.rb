8724995635924cc957df0d4d494661ef35c1769f
ZininSerge <zinin@xakep.ru>
2011-09-04 01:15:27 +0800

    Fix modules

--- lib/action_view/helpers/cache_helper.rb
+++ lib/action_view/helpers/cache_helper.rb
@@ -1,9 +1,12 @@
 module ActionView
-  module Helpers
-    module CacheHelper
-      def cache_for record, cacheable, options = {}, &block
-        record.fetch_cache_of(cacheable, options) { capture &block }
+  module CacheMachine
+    module Helpers
+      module CacheHelper
+        def cache_for record, cacheable, options = {}, &block
+          record.fetch_cache_of(cacheable, options) { capture &block }
+        end
       end
     end
   end
 end
+ActionView::Helpers::CacheHelper.send :extend, ActionView::CacheMachine::Helpers::CacheHelper
