3dc5dd2228f74b387c9de7fc6c3bbf92892cd8b9
ZininSerge <zinin@xakep.ru>
2011-09-04 04:06:01 +0800

    Remove dependencies

--- lib/cache_machine/helpers/cache_helper.rb
+++ lib/cache_machine/helpers/cache_helper.rb
@@ -1,12 +1,6 @@
-require 'action_view/helpers/capture_helper'
-require 'action_view/helpers/cache_helper'
-
 module CacheMachine
   module Helpers
     module CacheHelper
-
-      include ActionView::Helpers::CaptureHelper
-
       def cache_for record, cacheable, options = {}, &block
         record.fetch_cache_of(cacheable, options.merge(:format => :ehtml)) { capture &block }
       end
