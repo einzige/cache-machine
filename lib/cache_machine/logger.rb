65a3692a53237755bdb8f36771224db37d84c7f7
Kevin Goslar <kevin.goslar@gmail.com>
2011-09-13 18:14:43 -0700

    Logging output goes to the error stream.

--- lib/cache_machine/logger.rb
+++ lib/cache_machine/logger.rb
@@ -19,12 +19,16 @@ module CacheMachine
       # Call like this in your your code, best in development.rb: ActiveRecord::CacheMachine::Logger.level = :info
       def level= value
         @@level = LOGGING_LEVELS[value] or raise "CACHE_MACHINE: Unknown log level: '#{value}'."
-        puts "CACHE_MACHINE: Setting log level to '#{value}'."
+        if @@level <= LOGGING_LEVELS[:info]
+          STDERR.puts "CACHE_MACHINE: Setting log level to '#{value}'.\n"
+        end
       end
 
       # Logs the given entry with the given log level.
       def write level, text
-        puts text if @@level <= (LOGGING_LEVELS[level] or raise "CACHE_MACHINE: Unknown log level: '#{level}'.")
+        if @@level <= (LOGGING_LEVELS[level] or raise "CACHE_MACHINE: Unknown log level: '#{level}'.")
+          STDERR.puts text
+        end
       end
     end
   end
