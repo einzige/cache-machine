fbea4524c07e9c4c86ff9956ca1fc4860d823e52
ZininSerge <zinin@xakep.ru>
2012-03-01 20:46:34 +0800

    Fixes rake params

--- lib/cache_machine/tasks.rb
+++ lib/cache_machine/tasks.rb
@@ -1,6 +1,6 @@
 namespace :cache_machine do
   desc "Caches the map of associations."
-  task :fill_associations_map, [:models] => :environment do
+  task :fill_associations_map, [:models] => :environment do |t, args|
     models = args[:models] ? args[:models].map(&:constantize) : CacheMachine::Cache::Map.registered_models
     models.each do |model|
       CacheMachine::Cache::Map.fill_associations_map model
