37dba5202fa97815771701acf7b7805834e28050
ZininSerge <zinin@xakep.ru>
2012-03-02 17:49:10 +0800

    Fixes timestamp issue

--- lib/cache_machine/cache/timestamp.rb
+++ lib/cache_machine/cache/timestamp.rb
@@ -17,32 +17,13 @@ module CacheMachine
         #
         # @return [ String ]
         def timestamp
-          CacheMachine::Cache::timestamps_adapter.fetch(timestamp_key) { Time.now.to_i.to_s }
-        end
-
-        # Returns cache key to fetch timestamp from cache.
-        #
-        # @return [ String ]
-        def timestamp_key
-          "#{self.name}_timestamp"
-        end
-
-        # Returns cache key of anything with timestamp attached.
-        #
-        # @example Return timestamped key of the class.
-        #   MyActiveRecordClass.timestamped_key
-        #
-        # @return [ String ]
-        def timestamped_key
-          "#{timestamp_key}_#{timestamp}"
+          CacheMachine::Cache::timestamps_adapter.fetch_timestamp(self.name) { Time.now.to_i.to_s }
         end
 
         # Resets timestamp of class collection.
         #
         def reset_timestamp
-          cache_key = timestamp_key
-          CacheMachine::Logger.info "CACHE_MACHINE (reset_timestamp): deleting '#{cache_key}'."
-          CacheMachine::Cache::timestamps_adapter.reset_timestamp(cache_key)
+          CacheMachine::Cache::timestamps_adapter.reset_timestamp(self.name)
         end
       end
     end
